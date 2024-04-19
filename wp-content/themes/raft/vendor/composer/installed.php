f337477bec36987f018926907a3a759ddab7abc7
Maneetsodhi <sodh6008@mylaurier.ca>
2024-04-19 15:51:58 -0400

    make custom element

---
+++ wp-content/themes/raft/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/raft',
+        'pretty_version' => 'v1.1.4',
+        'version' => '1.1.4.0',
+        'reference' => '7846ef8891d990b1b56949aeea2eb97abafacf48',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/raft' => array(
+            'pretty_version' => 'v1.1.4',
+            'version' => '1.1.4.0',
+            'reference' => '7846ef8891d990b1b56949aeea2eb97abafacf48',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.14',
+            'version' => '3.3.14.0',
+            'reference' => '662952078c57b12e4d3af9bc98ef847ea3500206',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ wp-content/themes/website-theme/customElements.js
@@ -0,0 +1,15 @@
+class CopyrightYear extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-year", CopyrightYear);
+
+class ChooseSide extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML = `(<a href="Kit-Maker">Kit Maker </a> <a href="Kit-Developer">Kit Developer</a>)`
+	}
+}
+
+customElements.define("x-choose", ChooseSide);
